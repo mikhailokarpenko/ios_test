b2fd973df5b4294f71d316aff3d96fb1ec4b9bd6
mike <qweqwenet@gmail.com>
2022-11-02 14:26:42 -0400

    Initial Commit

---
+++ TestAttributedStrings/TestAttributedStringsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAttributedStringsApp.swift
+//  TestAttributedStrings
+//
+//  Created by Mike Karpenko on 02.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct TestAttributedStringsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
