6e526eb554f13bde00bfbe5b1340bd13f7fdb06b
mike <qweqwenet@gmail.com>
2022-11-02 14:39:27 -0400

    init

---
+++ TestAttributedStrings/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  TestAttributedStrings
+//
+//  Created by Mike Karpenko on 02.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
